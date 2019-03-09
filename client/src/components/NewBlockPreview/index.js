9573cdfdcd56ae6bf76d58049c2da80a6d5a5669
rhoang57 <rhoang57@gmail.com>
2019-03-09 14:23:12 -0800

    add Links

--- client/src/components/NewBlockPreview/index.js
+++ client/src/components/NewBlockPreview/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 
 class NewBlockPreview extends Component {
   render() {
       return (
           <div className="card h-100">
-            <div className="new-card-title"><h2>START A NEW STORY<span className="add-block"><a href={this.props.newblocklink} className="add-block">    +</a></span></h2></div>
+            <div className="new-card-title"><h2>START A NEW STORY<span className="add-block"><Link to={this.props.newblocklink} className="add-block">    +</Link></span></h2></div>
             <div className="preview-text"><br /> <br /> <br /> <br /></div>
             <div className="container">
             <h6 className="word-count">Word Count: 0</h6>
