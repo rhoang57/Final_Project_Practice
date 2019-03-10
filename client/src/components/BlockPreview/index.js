9c4e55e5b28c0106032e0c6b671b90b950149f63
rhoang57 <rhoang57@gmail.com>
2019-03-09 16:39:44 -0800

    another try

--- client/src/components/BlockPreview/index.js
+++ client/src/components/BlockPreview/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./style.css";
-import LastTextPreview from "../LastTextPreview";
 import { Link } from "react-router-dom";
 
 
@@ -9,7 +8,7 @@ class BlockPreview extends Component {
       return (
           <div className="card h-100">
             <div className="card-title"><h3><i>{this.props.title}</i><span className="add-block"><Link to={this.props.newblocklink} className="add-block"><i className="fas fa-keyboard"></i></Link></span></h3></div>
-            <LastTextPreview excerpt={"testing"}/>
+            <div className="preview-text"><i>{this.props.previewText}</i></div>
             <div className="count-container w-100">
             <h6 className="word-count">Word Count:  {this.props.wordCount}</h6>
             <h6 className="contributor-count">No. of Authors:  {this.props.authorCount}</h6>
--- client/src/components/NewBlockPreview/index.js
+++ client/src/components/NewBlockPreview/index.js
@@ -10,8 +10,7 @@ class NewBlockPreview extends Component {
             <div className="new-card-title"><h2>START A NEW STORY<span className="add-block"><Link to={this.props.newblocklink} className="add-block">    +</Link></span></h2></div>
             <div className="preview-text"><br /> <br /> <br /> <br /></div>
             <div className="container">
-            <h6 className="word-count">Word Count: 0</h6>
-            <h6 className="contributor-count">No. of Contributors: 0</h6>
+
             </div>
           </div>
       )
